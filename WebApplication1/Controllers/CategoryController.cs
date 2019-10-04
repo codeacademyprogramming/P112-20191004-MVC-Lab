4e36fe8f7e67e2cfd7923d10425e54ef87992df1
Sattar Amrahov <sattar@code.local>
2019-10-04 12:54:11 +0400

    projects added

---
+++ WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        P112NewsEntities db = new P112NewsEntities();
+        // GET: Category
+        public ActionResult List()
+        {
+            List<Category> kateqoriyalar = db.Categories.ToList();
+            return View(kateqoriyalar);
+        }
+        public ActionResult Details(int id)
+        {
+            Category cat = db.Categories.FirstOrDefault(c => c.Id == id);
+            return View(cat);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/NewsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class NewsController : Controller
+    {
+
+        P112NewsEntities db = new P112NewsEntities();
+
+        // GET: News
+        public ActionResult Index()
+        {
+            List<News> news = db.News.ToList();
+            return View(news);
+        }
+
+        public ActionResult Xeber(int? xeberinIDsi)
+        {
+           News xeber =  db.News.FirstOrDefault(n => n.Id == xeberinIDsi);
+            return View(xeber);
+        }
+    }
+}
